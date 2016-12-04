de5bdad1ce8531a6df1a0c826ed440a7d93eae37
Bahaa Aidi <bahaa.aidi@gmail.com>
2016-12-04 22:53:13 +0200

    v0.0.8-4

--- dist/videojs5-hlsjs-source-handler.js
+++ dist/videojs5-hlsjs-source-handler.js
@@ -1,7 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-require('./videojs5-hlsjs-source-handler.js')(window, videojs, Hls);
-
-},{"./videojs5-hlsjs-source-handler.js":2}],2:[function(require,module,exports){
 var attachVideojsHolaProvider = function (window, videojs, Hls) {
     function HolaProviderHLS (source, tech) {
         tech.name_ = 'holaHLS';
@@ -146,14 +143,6 @@ var attachVideojsHolaProvider = function (window, videojs, Hls) {
                 list.push(levelData(-1, 'auto'));
             levels.forEach(function(level, index){
                 list.push(levelData(index, _levelLabel(level))); });
-            console.log({
-                quality: {
-                    list: list,
-                    selected: _hls.manualLevel,
-                    current: _hls.loadLevel,
-                },
-                callback: switchQuality,
-            });
             tech.trigger('loadedqualitydata', {
                 quality: {
                     list: list,
