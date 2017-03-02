b2bb7c677846577ed26db6f666e42ecc46e002ea
Bahaa Aidi <bahaa.aidi@gmail.com>
2017-03-02 15:47:21 +0200

    v0.0.8-22

--- dist/videojs5-hlsjs-source-handler.js
+++ dist/videojs5-hlsjs-source-handler.js
@@ -1,37 +1,58 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.HolaProviderHLS = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 var E = module.exports;
-var Hls, videojs;
+var hlsjsConfig;
+var attached = false, disabled = false;
+E.Hls = window.Hls;
+E.videojs = window.videojs;
 
-var sourceHandler = {
-    canHandleSource: function (source) {
-        var hlsTypeRE = /^application\/x-mpegURL$/i;
-        var hlsExtRE = /\.m3u8/i;
-        var result;
-        if (this.disabled) {
-            result = '';
-        } else if (hlsTypeRE.test(source.type)) {
-            result = 'probably';
-        } else if (hlsExtRE.test(source.src)) {
-            result = 'maybe';
-        } else {
-            result = '';
-        }
-        return result;
-    },
-    handleSource: function (source, tech) {
-        if (tech.hlsProvider) {
-            tech.hlsProvider.dispose();
-        }
-        tech.hlsProvider = new HolaProviderHLS(source, tech, this.hlsjsConfig);
-        return tech.hlsProvider;
-    },
-    name: 'HolaProviderHLS',
-    attached: false,
-    disabled: false,
+E.VERSION = '0.0.8-22';
+E.name = 'HolaProviderHLS';
+
+E.attach = function(obsolete_param, videojs, Hls, hlsjsConfig_){
+    if (Hls)
+        E.Hls = Hls;
+    if (videojs)
+        E.videojs = videojs;
+    if (hlsjsConfig_)
+        hlsjsConfig = hlsjsConfig_;
+    if (attached)
+        disabled = false;
+    else if (E.Hls.isSupported())
+    {
+        attached = true;
+        disabled = false;
+        E.videojs.getComponent('Html5').registerSourceHandler(E, 0);
+        E.videojs.HolaProviderHLS = HolaProviderHLS;
+        console.log('HolaProviderHLS registerd as Html5 SourceHandler');
+    }
+    else
+        console.error('Hls.js is not supported in this browser!');
 };
 
-function HolaProviderHLS(source, tech, hlsjsConfig) {
+E.detach = function(){
+    // we don't unregister source handler, just set it as disabled so it will
+    // return false in canHandleSource()
+    disabled = true;
+};
+
+E.canHandleSource = function(source){
+    if (disabled)
+        return '';
+    if (/^application\/x-mpegURL$/i.test(source.type))
+        return 'probably';
+    if (/\.m3u8/i.test(source.src))
+        return 'maybe';
+    return '';
+};
+
+E.handleSource = function(source, tech){
+    if (tech.hlsProvider)
+        tech.hlsProvider.dispose();
+    return tech.hlsProvider = new HolaProviderHLS(source, tech);
+};
+
+function HolaProviderHLS(source, tech){
     tech.name_ = 'holaHLS';
     var _video = tech.el();
     var _hls;
@@ -39,9 +60,11 @@ function HolaProviderHLS(source, tech, hlsjsConfig) {
     var _duration = null;
     var _seekableStart = 0;
     var _seekableEnd = 0;
-    _video.addEventListener('error', function(evt) {
-        var errorTxt,mediaError=evt.currentTarget.error;
-        switch(mediaError.code) {
+
+    _video.addEventListener('error', function(evt){
+        var errorTxt, mediaError = evt.currentTarget.error;
+        switch(mediaError.code)
+        {
         case mediaError.MEDIA_ERR_ABORTED:
             errorTxt = 'You aborted the video playback';
             break;
@@ -59,51 +82,52 @@ function HolaProviderHLS(source, tech, hlsjsConfig) {
         console.error('MEDIA_ERROR: ', errorTxt);
     });
 
-    function initialize() {
-        tech.hls_obj = _hls = new Hls(videojs.mergeOptions(
+    function initialize(){
+        tech.hls_obj = _hls = new E.Hls(E.videojs.mergeOptions(
             tech.options_.hlsjsConfig, hlsjsConfig));
         _hls.manual_level = -1;
-        _hls.on(Hls.Events.ERROR, function(event, data) {
+        _hls.on(E.Hls.Events.ERROR, function(event, data){
             _onError(event, data, tech, _errorCounts);
         });
-        _hls.on(Hls.Events.LEVEL_SWITCH, updateQuality);
-        _hls.on(Hls.Events.LEVEL_LOADED, function(event, data) {
-            _duration = data.details.live ? Infinity : data.details.totalduration;
+        _hls.on(E.Hls.Events.LEVEL_SWITCH, updateQuality);
+        _hls.on(E.Hls.Events.LEVEL_LOADED, function(event, data){
+            _duration = data.details.live ? Infinity :
+                data.details.totalduration;
         });
-        _hls.on(Hls.Events.LEVEL_UPDATED, function(event, data) {
+        _hls.on(E.Hls.Events.LEVEL_UPDATED, function(event, data){
             _seekableStart = data.details.live ?
                 data.details.fragments[0].start : 0;
             _seekableEnd = data.details.live ?
                 _hls.streamController.computeLivePosition(_seekableStart,
                 data.details) : data.details.totalduration;
         });
-        _hls.on(Hls.Events.FRAG_PARSING_METADATA, function(event, data) {
+        _hls.on(E.Hls.Events.FRAG_PARSING_METADATA, function(event, data){
             tech.trigger('parsedmetadata', data);
         });
         _hls.attachMedia(_video);
     }
 
-    this.duration = function () {
+    this.duration = function(){
         return _duration || _video.duration || 0;
     };
 
     this.seekable = function(){
-        return videojs.createTimeRanges([[_seekableStart, _seekableEnd]]);
+        return E.videojs.createTimeRanges([[_seekableStart, _seekableEnd]]);
     };
 
-    this.dispose = function () {
+    this.dispose = function(){
         _hls.destroy();
     };
 
-    function load(source) {
+    function load(source){
         _hls.loadSource(source.src);
     }
 
-    function switchQuality(qualityId) {
+    function switchQuality(qualityId){
         _hls.manual_level = qualityId;
         if (_hls.hola_adaptive)
         {
-            (_video.player||videojs.getPlayers()[_video.playerId])
+            (_video.player||E.videojs.getPlayers()[_video.playerId])
             .trigger('mediachange');
         }
         else
@@ -111,43 +135,54 @@ function HolaProviderHLS(source, tech, hlsjsConfig) {
         updateQuality();
     }
 
-    function _handleMediaError(error) {
-        if (_errorCounts[Hls.ErrorTypes.MEDIA_ERROR] === 1) {
+    function _handleMediaError(error){
+        if (_errorCounts[E.Hls.ErrorTypes.MEDIA_ERROR] === 1)
+        {
             console.info('trying to recover media error');
             _hls.recoverMediaError();
-        } else if (_errorCounts[Hls.ErrorTypes.MEDIA_ERROR] === 2) {
+        }
+        else if (_errorCounts[E.Hls.ErrorTypes.MEDIA_ERROR] === 2)
+        {
             console.info('2nd try to recover media error (by swapping audio codec');
             _hls.swapAudioCodec();
             _hls.recoverMediaError();
-        } else if (_errorCounts[Hls.ErrorTypes.MEDIA_ERROR] > 2) {
+        }
+        else if (_errorCounts[E.Hls.ErrorTypes.MEDIA_ERROR] > 2)
+        {
             console.info('bubbling media error up to VIDEOJS');
             error.code = 3;
-            tech.error = function() { return error; };
+            tech.error = function(){ return error; };
             tech.trigger('error');
         }
     }
 
-    function _onError(event, data) {
+    function _onError(event, data){
         var level = 'error';
         var error = {
             message: ('HLS.js error: ' + data.type + ' - fatal: ' + data.fatal + ' - ' + data.details),
         };
-        if (!data.fatal && data.details === Hls.ErrorDetails.BUFFER_STALLED_ERROR) {
+        if (!data.fatal &&
+            data.details === E.Hls.ErrorDetails.BUFFER_STALLED_ERROR)
+        {
             level = 'warn';
         }
         console[level](error.message);
         // increment/set error count
-        _errorCounts[data.type] ? _errorCounts[data.type] += 1 : _errorCounts[data.type] = 1;
-        // implement simple error handling based on hls.js documentation (https://github.com/dailymotion/hls.js/blob/master/API.md#fifth-step-error-handling)
-        if (data.fatal) {
-            switch (data.type) {
-            case Hls.ErrorTypes.NETWORK_ERROR:
+        _errorCounts[data.type] ?
+            _errorCounts[data.type] += 1 : _errorCounts[data.type] = 1;
+        // implement simple error handling based on hls.js documentation
+        // (https://github.com/dailymotion/hls.js/blob/master/API.md#fifth-step-error-handling)
+        if (data.fatal)
+        {
+            switch (data.type)
+            {
+            case E.Hls.ErrorTypes.NETWORK_ERROR:
                 console.info('bubbling network error up to VIDEOJS');
                 error.code = 2;
                 tech.error = function() { return error; };
                 tech.trigger('error');
                 break;
-            case Hls.ErrorTypes.MEDIA_ERROR:
+            case E.Hls.ErrorTypes.MEDIA_ERROR:
                 _handleMediaError(error);
                 break;
             default:
@@ -178,16 +213,19 @@ function HolaProviderHLS(source, tech, hlsjsConfig) {
         return str.replace(/\.0*$/, '')+sizes[i];
     }
 
-    function _levelLabel(level) {
-        if (level.height) return level.height + 'p';
-        else if (level.width) return Math.round(level.width * 9 / 16) + 'p';
-        else if (level.bitrate) return scaledNumber(level.bitrate) + 'bps';
-        else return 0;
+    function _levelLabel(level){
+        if (level.height)
+            return level.height + 'p';
+        if (level.width)
+            return Math.round(level.width * 9 / 16) + 'p';
+        if (level.bitrate)
+            return scaledNumber(level.bitrate) + 'bps';
+        return 0;
     }
 
     function levelData(id, label){ return {id: id, label: label}; }
 
-    function updateQuality() {
+    function updateQuality(){
         var list = [], levels = _hls.levels;
         if (levels.length > 1)
             list.push(levelData(-1, 'auto'));
@@ -207,33 +245,6 @@ function HolaProviderHLS(source, tech, hlsjsConfig) {
     load(source);
 }
 
-function attachHolaProviderHLS(window, videojs_, Hls_, hlsjsConfig) {
-    Hls = Hls_;
-    videojs = videojs_;
-    if (hlsjsConfig)
-        sourceHandler.hlsjsConfig = hlsjsConfig;
-    if (sourceHandler.attached) {
-        sourceHandler.disabled = false;
-    } else if (Hls.isSupported()) {
-        sourceHandler.attached = true;
-        sourceHandler.disabled = false;
-        videojs.getComponent('Html5').registerSourceHandler(sourceHandler, 0);
-        videojs.HolaProviderHLS = HolaProviderHLS;
-        console.log('HolaProviderHLS registerd as Html5 SourceHandler');
-    } else {
-        console.error('Hls.js is not supported in this browser!');
-    }
-}
-
-function detachHolaProviderHLS() {
-    // we don't unregister source handler, just set it as disabled so it will
-    // return false in canHandleSource()
-    sourceHandler.disabled = true;
-}
-
-E.attach = attachHolaProviderHLS;
-E.detach = detachHolaProviderHLS;
-E.VERSION = '0.0.8-21';
 
 },{}]},{},[1])(1)
 });
