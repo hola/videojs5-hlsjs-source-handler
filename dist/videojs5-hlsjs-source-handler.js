915a5f660d87bdeb615e6b4390e7e59a84abea42
Volodymyr Tytar <volodymyr.tytar@gmail.com>
2017-06-05 22:00:09 +0300

    0.0.8-49

--- dist/videojs5-hlsjs-source-handler.js
+++ dist/videojs5-hlsjs-source-handler.js
@@ -2,93 +2,12 @@
 'use strict';
 var E = module.exports;
 var hlsjsConfig;
-var ls;
-try { ls = window.localStorage; } catch(e){}
 var attached = false, disabled = false;
 E.Hls = window.Hls;
 E.videojs = window.videojs;
 
-E.VERSION = '0.0.8-47';
+E.VERSION = '0.0.8-49';
 E.name = 'HolaProviderHLS';
-
-var script_conf = (function script_conf_init(){
-    // XXX vadiml copied from pkg/util/conv.js to avoid dependency
-    function parse_leaf(v, opt){
-        if (!v || typeof v!='object' || Object.keys(v).length!=1)
-            return v;
-        if (v.__Function__ && opt.func)
-            return new Function('', '"use strict";return ('+v.__Function__+');')();
-        if (v.__RegExp__ && opt.re)
-        {
-            var parsed = /^\/(.*)\/(\w*)$/.exec(v.__RegExp__);
-            if (!parsed)
-                throw new Error('failed parsing regexp');
-            return new RegExp(parsed[1], parsed[2]);
-        }
-        return v;
-    }
-    function parse_obj(v, opt){
-        if (!v || typeof v!='object')
-            return v;
-        if (Array.isArray(v))
-        {
-            for (var i = 0; i<v.length; i++)
-                v[i] = parse_obj(v[i], opt);
-            return v;
-        }
-        var v2 = parse_leaf(v, opt);
-        if (v2!==v)
-            return v2;
-        for (var key in v)
-            v[key] = parse_obj(v[key], opt);
-        return v;
-    }
-    // XXX pavlo: workaround for UglifyJS optimization
-    function is_set(v) { return v==1; }
-    var attrs = {register: 'register-percent', manual_init: 'manual-init'};
-    var script = document.currentScript||
-        document.querySelector('#hola_videojs_hls_provider');
-    if (!script)
-        return {};
-    var rpercent = '{[=it.HOLA_REGISTER_PERCENT]}';
-    if (!rpercent.indexOf('{['))
-    {
-        if (!script.hasAttribute(attrs.register))
-            return {};
-        rpercent = +script.getAttribute(attrs.register);
-    }
-    if (isNaN(rpercent)||rpercent<0||rpercent>100)
-    {
-        console.error(E.name+': invalid '+attrs.register+' attribute, '+
-            'expected a value between 0 and 100 but '+
-            script.getAttribute(attrs.register)+' found');
-        return {disabled: true};
-    }
-    var embedded = is_set('{[=it.HOLA_EMBEDDED_PROVIDER]}');
-    // loader.js takes percent control on its side
-    if (embedded)
-        rpercent = 100;
-    if (window.location.search && window.URLSearchParams)
-    {
-        var params = new window.URLSearchParams(window.location.search);
-        rpercent = +params.get('hola_provider_register_percent')||rpercent;
-    }
-    if (ls && ls.getItem('hola_provider_register_percent'))
-    {
-        rpercent = +ls.getItem('hola_provider_register_percent');
-        console.info(E.name+': '+attrs.register+' forced to '+rpercent+
-            '% by localStorage configuration');
-    }
-    var autoinit = !embedded && !script.hasAttribute(attrs.manual_init);
-    var hls_params_str = '{[=it.HOLA_HLS_PARAMS]}';
-    var hls_params = {};
-    try {
-        hls_params = parse_obj(JSON.parse(hls_params_str),
-            {func: true, re: true});
-    } catch(e){}
-    return {autoinit: autoinit, hls_params: hls_params,
-        disabled: !rpercent||Math.random()*100>rpercent};
-})();
 
 E.attach = function(obsolete_param, videojs, Hls, hlsjsConfig_){
     if (Hls)
@@ -226,7 +145,7 @@ function HolaProviderHLS(source, tech){
     function initialize(){
         var hola_log, hls_params = {};
         Object.assign(hls_params, hlsjsConfig);
-        Object.assign(hls_params, script_conf.hls_params);
+        Object.assign(hls_params, E.hls_params||{});
         if (hls_params.debug!==undefined)
             hola_log = hls_params.debug;
         hls_params.debug = {};
@@ -409,11 +328,6 @@ function HolaProviderHLS(source, tech){
     if (_preload)
         load(source);
 }
-
-if (script_conf.disabled)
-    E.attach = E.detach = function(){};
-else if (script_conf.autoinit)
-    E.attach();
 
 },{}]},{},[1])(1)
 });
